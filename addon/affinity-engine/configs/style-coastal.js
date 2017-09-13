d67e6b18d07db8eb7c160bd9e26956891efd25b4
null null null <null@affinitygamescollective.org>
2017-09-13 13:18:27 -0400

    prevent default config conflicts with user config

--- addon/affinity-engine/configs/style-coastal.js
+++ addon/affinity-engine/configs/style-coastal.js
@@ -1,9 +1,11 @@
 export default {
   priority: 1.2,
-  prompt: {
-    attrs: {
-      classNames: {
-        decorative: ['ae-coastal']
+  default: {
+    prompt: {
+      attrs: {
+        classNames: {
+          decorative: ['ae-coastal']
+        }
       }
     }
   }
